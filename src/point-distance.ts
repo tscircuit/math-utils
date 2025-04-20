f8c0fdd68bfcca66850c4c55202a9eb9e88b3d4a
seveibar <seveibar@gmail.com>
2025-04-19 21:37:13 -0700

    add midpoint method

--- src/point-distance.ts
+++ src/point-distance.ts
@@ -50,3 +50,10 @@ export function pointToBoundsDistance(p: Point, bounds: Bounds): number {
   // Calculate the distance to the closest point
   return distance(p, { x: closestX, y: closestY })
 }
+
+export function midpoint(p1: Point, p2: Point): Point {
+  return {
+    x: (p1.x + p2.x) / 2,
+    y: (p1.y + p2.y) / 2,
+  }
+}
